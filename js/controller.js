9b04dab466ae347cd86965de3708e815f5fc0671
jonatan <jonatanmarcelyn@gmail.com>
2021-03-20 16:32:08 -0300

    second commit

--- js/controller.js
+++ js/controller.js
@@ -1,8 +1,19 @@
-var nome = "Marcelyn Pereira";
-var peso = 76.00;
-var altura = 1.78;
-var imc = peso / (altura**2);
+var pacientes = []; //vetor vazio
+var botao = document.getElementById("calcular"); //<button type="button" id="calcular" ...
+botao.addEventListener('click', function() {
+                                    var paciente = lePaciente(); //lê e retorna o objeto
+                                    pacientes.push( paciente );
+                                    mostraPaciente( paciente );
+                                });
 
+var botao = document.getElementById("relatorio");
+botao.addEventListener('click', function() {
+                                    gerarRelatorio( pacientes );
+                                });
+
+/*
 console.log(nome);
 console.log(`Peso: ${peso} - Altura: ${altura}`);
 console.log(`Cálculo do IMC: ${imc.toFixed(2)}`);
+*/
+
