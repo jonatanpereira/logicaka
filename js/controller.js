8d68c4958919614ddc1affa66807141c155e59f7
jonatan <jonatanmarcelyn@gmail.com>
2021-03-20 12:17:59 -0300

    first commit

---
+++ js/controller.js
@@ -0,0 +1,8 @@
+var nome = "Marcelyn Pereira";
+var peso = 76.00;
+var altura = 1.78;
+var imc = peso / (altura**2);
+
+console.log(nome);
+console.log(`Peso: ${peso} - Altura: ${altura}`);
+console.log(`Cálculo do IMC: ${imc.toFixed(2)}`);
